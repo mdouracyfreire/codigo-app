18144bb3ee300c2f42db77240781ab41cf4f89c1
Douracy Freire <mdouracy.fc@gmail.com>
2026-05-15 19:54:30 -0300

    applying javascript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Criando um alerta na minha página");
+let campoTarefa = document.querySelector(".adicionar-tarefa input");
+let botaoAdicionar = document.querySelector(".adicionar-tarefa button");
+let tarefa = [];
+
+botaoAdicionar.onclick = adicionarTarefa;
+
+function adicionarTarefa() {}
+function removerTarefa() {}
+function listarTarefa() {}
